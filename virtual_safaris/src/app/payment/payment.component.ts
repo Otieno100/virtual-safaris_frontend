b5b83527f6abe3f26ccb25d35396ed9bc80e5b8a
albright <sungutialbright@gmail.com>
2022-07-07 11:35:06 +0300

    add payment component files

--- virtual_safaris/src/app/payment/payment.component.ts
+++ virtual_safaris/src/app/payment/payment.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentComponent implements OnInit {
 
-  constructor() { }
+  paymentForm = FormGroup
+
+  constructor( private formBuilder : FormBuilder,
+              private http : HttpClient,
+              private router: Router) {}
 
   ngOnInit(): void {
+   this.paymentForm = this.formBuilder.group({
+      number: ['', Validators.required],
+      amount: ['', Validators.required],
+  })
+  
   }
 
+  submit():void{
+    console.log(this.paymentForm.getRawValue());
+    this.http.post('http://localhost:8000/api/v1/online/lipa/<phonenumber>/<amount>/',this.paymentForm.getRawValue())
+    .subscribe(() => this.router.navigate(['/']));
+     
+  }
 }
+
+
